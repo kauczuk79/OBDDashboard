4e082b45042642a68be67453af027378e62d2024
Wesolowski, Robert <robert.wesolowski@gft.com>
2016-12-09 14:28:36 +0100

    Prepare gulp tasks

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -1,5 +1,41 @@
 var gulp = require('gulp');
 
-gulp.task('default', function () {
-    console.log('Default task')
+gulp.task('default', [
+    'compile',
+    'concatenate',
+    'minify'
+]);
+
+gulp.task('compile', [
+    'compile:style'
+]);
+
+gulp.task('concatenate', [
+    'concatenate:js',
+    'concatenate:style'
+])
+
+gulp.task('minify', [
+    'minify:js',
+    'minify:style'
+]);
+
+gulp.task('compile:style', function () {
+    console.log('Compile Styles');
+});
+
+gulp.task('concatenate:js', function () {
+    console.log('Concatenate JavaScript');
+});
+
+gulp.task('concatenate:style', function () {
+    console.log('Concatenate Styles');
+});
+
+gulp.task('minify:js', function () {
+    console.log('Minify JavaScript');
+});
+
+gulp.task('minify:style', function () {
+    console.log('Minify Styles');
 });
